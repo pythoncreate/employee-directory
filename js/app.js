a55ececfc9996961fb7202560e5d93e79ba04b13
Chris Stuart <crstuart@gmail.com>
2017-10-10 13:09:08 -0400

    almost there

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ $.ajax({
     success: function(data){
     	employees = data.results;
     	displayEmployees(employees);
+    	$('.extra-info').hide();
 	}
 });
 //Create Function to Build Employee Car
@@ -14,25 +15,28 @@ function displayEmployees(employees){
 	var employeesHTML = ""
 	$.each(employees, function(i, employee) {
 		employeesHTML += '<div class="employee">';
-	    employeesHTML += '<a href="">';
 	    employeesHTML += '<img class="employee-photo" src="' + employee.picture.large + '"></a>';
 	    employeesHTML += '<div class="info">';
 	    employeesHTML += '<div class="name">'+ employee.name.first + ' ' + employee.name.last + '</div>';
 	    employeesHTML += '<div class="email grey-font">'+ employee.email + '</div>';
-	    employeesHTML += '<div class="city grey-font">' + employee.location.city + '</div></div></div>';
-	    
+	    employeesHTML += '<div class="city grey-font">' + employee.location.city + '</div></div>';
+	    employeesHTML += '<div class="extra-info"><hr align="left" width="90%">';
+	    employeesHTML += '<div class="phone">' + employee.phone + '</div>';
+	    employeesHTML += '<div class="address">' + employee.location.street + ' ' + employee.location.city;
+	    employeesHTML += ',' + employee.location.state + ' ' + employee.location.zip + '</div>';
+	    employeesHTML += '<div class="birthday">Birthday: ' + employee.location.dob + '</div></div></div>';
+ 
 	       });
 
     $('.employees').html(employeesHTML);
-
+    return phone;
 };
 
 //Create Function to Build Modal
 function displayModal(employees){
 	var employeesModal="";
 	//create modal
-    employeesModal += '<div>' + $(employees.phone).html() + '</div>';
-
+    employeesModal += $(employees).html();
     $('.modal-text').html(employeesModal);
 }
 
@@ -40,8 +44,10 @@ function displayModal(employees){
 var modal = document.getElementById('myModal');
 $('.employees').on("click", ".employee", function() {
 	  var current = $(this);
-	    modal.style.display = "block";
-	    displayModal(current);
+	  var extra = current.parent().find(".extra-info");
+	  extra.css('display', 'block');
+	  modal.style.display = "block";
+	  displayModal(current);
 });
 
 // // When the user clicks on (x), close the modal
